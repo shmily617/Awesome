caf93e08b9aef1b9ac82ac4ec7e1d4eaeb0e5846
mj LIU <mjliu@wsnterminaltekiiMac.local>
2016-08-05 20:54:32 +0800

    dataJSON

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 /**
- * Sample gouwuche App
- * date:2016-08-03
+ * Sample ListView sticky App
+ * date:2016-08-02
  * @Auth:mjliu
  */
 
@@ -10,349 +10,200 @@ import {
   StyleSheet,
   Text,
   View,
-  Image,
-  NavigatorIOS,
-  ScrollView,
-  AsyncStorage,
-  TouchableOpacity,
+  ListView,
+  ActivityIndicatorIOS
 } from 'react-native';
 
-var Model = [
-    {
-        id: '1',
-        title:'水蜜桃',
-        desc:'3个装',
-        price: 8.24,
-        url:'http://imgcdn.xuxian.com/upload/2016/07/19/20160719112733238_464_261.jpg'
-    },
-    {
-        id: '2',
-        title:'火龙果',
-        desc:'1个装',
-        price: 6.99,
-        url:'http://imgcdn.xuxian.com/upload/2016/04/08/20160408054321272_464_261.jpg'
-    },
-    {
-        id: '3',
-        title:'翡翠蜜瓜',
-        desc:'1个装',
-        price: 27.88,
-        url:'http://imgcdn.xuxian.com/upload/2016/08/01/20160801080632518_464_261.jpg'
-    },
-    {
-        id: '4',
-        title:'猕猴桃',
-        desc:'3个装',
-        price: 8.24,
-        url:'http://imgcdn.xuxian.com/upload/2016/03/31/20160331085551595_464_261.jpg'
-    },
-    {
-        id: '5',
-        title:'牛油果',
-        desc:'1个装',
-        price: 6.99,
-        url:'http://imgcdn.xuxian.com/upload/2016/07/14/20160714115959501_464_261.jpg'
-    },
-    {
-        id: '6',
-        title:'椰子',
-        desc:'1个装',
-        price: 27.88,
-        url:'http://imgcdn.xuxian.com/upload/2016/04/01/20160401122058619_464_261.jpg'
-    },
-    {
-      id: '7',
-      title: '佳沛新西兰进口猕猴桃',
-      desc: '12个装',
-      price: 99,
-      url: 'http://vczero.github.io/ctrip/guo_1.jpg'
-    },
-    {
-      id:'8',
-      title: '墨西哥进口牛油果',
-      desc: '6个装',
-      price: 59,
-      url: 'http://vczero.github.io/ctrip/guo_2.jpg'
-    },
-    {
-      id:'9',
-      title: '美国加州进口车厘子',
-      desc: '1000g',
-      price: 91.5,
-      url: 'http://vczero.github.io/ctrip/guo_3.jpg'
-    },
-    {
-      id:'10',
-      title: '新疆特产西梅',
-      desc: '1000g',
-      price: 69,
-      url: 'http://vczero.github.io/ctrip/guo_4.jpg'
-    },
-    {
-      id:'11',
-      title: '陕西大荔冬枣',
-      desc: '2000g',
-      price: 59.9,
-      url: 'http://vczero.github.io/ctrip/guo_5.jpg'
-    },
-    {
-      id:'12',
-      title: '南非红心西柚',
-      desc: '2500g',
-      price: 29.9,
-      url: 'http://vczero.github.io/ctrip/guo_6.jpg'
-    }
-];
+var API_URL = 'http://demo9383702.mockable.io/users';
 
-class Item extends Component {
+// import dataHome from './dataHome.js'
+
+class Awesome extends Component {
+  // Initialize the hardcoded data
+  constructor(props) {
+    super(props);
+
+    var getSectionData = (dataBlob, sectionID) => {
+      return dataBlob[sectionID];
+    };
+
+    var getRowData = (dataBlob, sectionID, rowID) => {
+      return dataBlob[sectionID+':'+rowID];
+    };
+
+    this.state = {
+      loaded:false,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2,
+        sectionHeaderHasChanged: (s1, s2) => s1 !==s2,
+        getSectionHeaderData: getSectionData,
+        getRowData: getRowData,
+      }),
+    };
+  }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    
     return (
-      <View style={styles.item}>
-        <TouchableOpacity onPress={this.props.press}>
-          <Image 
-              resizeMode="contain" 
-              style={styles.img}
-              source={{uri:this.props.url}}>
-            <Text numberOfLines={1} style={styles.item_text}>{this.props.title}</Text>
-          </Image>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.header} >
+          <Text style={styles.headerText} >User List</Text>
+        </View>
+      
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+          renderSectionHeader={this.renderSectionHeader}
+          style={styles.listView}
+        />
       </View>
     );
   }
-}
-
-class List extends Component {
-  constructor(props) {
-    super(props);
-     this.state = {
-       count: 0,
-     };
-  }
-  render() {
-    let list = [];
-    for(let i in Model){
-      console.log("这是第"+i);
-      if(i % 2 === 0){
-        console.log(i);
-        let row = (
-          <View style={styles.row} key={i}>
-            <Item 
-              url={Model[i].url} 
-              title={Model[i].title} 
-              press={this.press.bind(this, Model[i])}></Item>
-            <Item 
-              url={Model[parseInt(i)+1].url} 
-              title={Model[parseInt(i)+1].title} 
-              press={this.press.bind(this, Model[parseInt(i)+1])}></Item>
-          </View>
-          );
-        list.push(row);
-      }
-    }
-    let count = this.state.count;
-    let str = null;
-    if(count){
-      str = '，共'+ count + '件商品';
-    }
+  renderLoadingView() {
     return (
-      <ScrollView style={{marginTop:10}}>
-        {list}
-        <Text onPress={this.goGouWu.bind(this)} style={styles.btn}>去结算{str}</Text>
-      </ScrollView>
-    );
-  }
-
-  goGouWu(){
-      this.props.navigator.push({
-        component: GouWu,
-        title:'购物车'
-      });
-  }
-
-  press(data){
-      //改变数字状态
-      this.setState({
-        count: this.state.count+1,
-      });
-      //AsyncStorage存储
-      AsyncStorage.setItem('SP-' + this.genId() + '-SP', JSON.stringify(data), function(err){
-        if(err){
-          //TODO：存储出错
-          alert(err);
-        } else{
-          //成功
-        }
-      });
-
-  }
-
-//生成随机ID：GUID
-  genId(){
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        let r = Math.random() * 16 | 0,
-          v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-      }).toUpperCase();
-  }
-}
-
-class GouWu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      price: 0
-    };
-  }
-  render() {
-    let data = this.state.data;
-    let price = this.state.price;
-    let list = [];
-    for(var i in data){
-      price += parseFloat(data[i].price);
-      list.push(
-        <View style={[styles.row, styles.list_item]} key={i}>
-          <Text style={styles.list_item_desc}>
-            {data[i].title}
-            {data[i].desc}
-          </Text>
-          <Text style={styles.list_item_price}>¥{data[i].price}</Text>
+      <View style={styles.container} >
+        <View style={styles.header}>
+          <Text style={styles.headerText}>User List</Text>
         </View>
-      );
-    }
-    let str = null;
-    if(price){
-      str = '，共' + price.toFixed(1) + '元';
-    }
-    return(
-      <ScrollView style={{marginTop:10}}>
-        {list}
-        <Text style={styles.btn}>支付{str}</Text>
-        <Text style={styles.clear} onPress={this.clearStorage.bind(this)}>清空购物车</Text>
-      </ScrollView>
-    );
+        <View style={[styles.container,styles.center]}>
+              <ActivityIndicatorIOS
+                  animating={!this.state.loaded}
+                  style={[styles.activityIndicator, {height: 80}]}
+                  size="large"
+              />
+        </View>
+      </View>
+      //android
+      // <View style={[styles.container,styles.center]}>
+      //   <Text>正在加载电影数据</Text>
+      // </View>
+    )
   }
-  componentDidMount() {
-    let _that = this;
-    AsyncStorage.getAllKeys(function(err, keys){
-      if(err){
-        //TODO：存储取数据出错
-        //如果发生错误，这里直接返回（return）防止进入下面的逻辑
-      }
-      AsyncStorage.multiGet(keys, function(errs, result){
-        //TODO：错误处理
-        //得到的结果是二维数组
-        //result[i][0]表示我们存储的键，result[i][1]表示我们存储的值
-        let arr = [];
-        for(let i in result){
-          arr.push(JSON.parse(result[i][1]));
-        }
-        _that.setState({
-          data: arr
-        });
-      });     
-    });
-  }
-  clearStorage(){
-    let _that = this;
-    AsyncStorage.clear(function(err){
-      if(!err){
-        _that.setState({
-          data:[],
-          price: 0
-        });
-        alert('购物车已经清空');
-      }
-      //TODO:ERR
-    });
-  }
-}
 
-class Awesome extends Component {
-  render() {
+  renderSectionHeader(sectionData, sectionID) {
     return (
-      <NavigatorIOS
-        style={styles.container}
-        initialRoute={
-          {
-            component: List,
-            title: '水果列表'
-          }
-        }/>
+      <View style={styles.section}>
+        <Text style={styles.text}>片头-公司：{sectionData}-{sectionID}</Text>
+      </View>
     );
+  }
+
+  renderRow(rowData, sectionID, rowID) {
+    return (
+      <View style={styles.rowStyle}>
+        <Text style={styles.rowText}>{rowData.name.title}_{rowData.name.first}_{rowData.name.last}-{sectionID}-{rowID}</Text>
+      </View>
+    );
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+  
+  fetchData() {
+    fetch(API_URL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      // var responseData = dataHome;
+      var organizations = responseData.results,
+          length = organizations.length,
+          dataBlob = {},
+          sectionIDs = [],
+          rowIDs = [],
+          organization,
+          users,
+          userLength,
+          user,
+          i,
+          j;
+
+      for (i = 0; i < length; i++) {
+          organization = organizations[i];
+
+          sectionIDs.push(organization.id);
+          dataBlob[organization.id] = organization.organization;
+
+          users = organization.users;
+          userLength = users.length;
+          
+          rowIDs[i] = [];//二维数组
+
+          for(j = 0; j < userLength; j++) {
+              user = users[j].user;
+              rowIDs[i].push(user.md5);
+
+              dataBlob[organization.id + ':' + user.md5] = user;
+          }
+      }
+
+      this.setState({
+          dataSource : this.state.dataSource.cloneWithRowsAndSections(dataBlob, sectionIDs, rowIDs),
+          loaded     : true
+      });
+    })
+    .done();     
   }
 }
 
 const styles = StyleSheet.create({
- container: {
+  listView:{
+    backgroundColor: '#F5FCFF',
+  },
+  container: {
     flex: 1,
   },
-  row:{
-    flexDirection: 'row',
-    marginBottom: 10,
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  item:{
-    flex:1,
-    marginLeft:5,
-    borderWidth:1,
-    borderColor:'#ddd',
-    marginRight:5,
-    height:100,
-  }, 
-  img:{
-    flex:1,
-    backgroundColor: 'transparent'
+  activityIndicator: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  item_text:{
-    backgroundColor: '#000',
-    opacity: 0.7,
-    color:'#fff',
-    height:25,
-    lineHeight:18,
-    textAlign:'center',
-    marginTop:74
+  header: {
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3F51B5',
+    flexDirection: 'column',
+    paddingTop: 25
   },
-    btn:{
-    backgroundColor:'#FF7200',
-    height:33,
-    textAlign:'center',
-    color:'#fff',
-    marginLeft:10,
-    marginRight:10,
-    lineHeight:24,
-    marginTop:40,
-    fontSize:18,
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'white'
   },
-  list_item:{
-    marginLeft:5,
-    marginRight:5,
-    padding:5,
-    borderWidth:1,
-    height:30,
-    borderRadius:3,
-    borderColor:'#ddd'
+  text: {
+    color: 'white',
+    paddingHorizontal: 8,
+    fontSize: 16
   },
-  list_item_desc:{
-    flex:2,
-    fontSize:15
+  rowStyle: {
+    paddingVertical: 20,
+    paddingLeft: 16,
+    borderTopColor: 'white',
+    borderLeftColor: 'white',
+    borderRightColor: 'white',
+    borderBottomColor: '#E0E0E0',
+    borderWidth: 1
   },
-  list_item_price:{
-    flex:1, 
-    textAlign:'right',
-    fontSize:15
+  rowText: {
+    color: '#212121',
+    fontSize: 16
   },
-  clear:{
-    marginTop:10,
-    backgroundColor:'#FFF',
-    color:'#000',
-    borderWidth:1,
-    borderColor:'#ddd',
-    marginLeft:10,
-    marginRight:10,
-    lineHeight:24,
-    height:33,
-    fontSize:18,
-    textAlign:'center',
+  subText: {
+    fontSize: 14,
+    color: '#757575'
+  },
+  section: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    padding: 6,
+    backgroundColor: '#2196F3'
   }
 });
 
